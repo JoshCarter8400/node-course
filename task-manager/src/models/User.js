36325d65ae75a1a23038b5431590937994cb6f2d
Josh Carter <joshcarter8400@gmail.com>
2021-03-11 15:06:05 -0500

    working on setting up user and task endpoints

---
+++ task-manager/src/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Invalid credentials');
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 6,
+    validate(value) {
+      if (value.includes('password')) {
+        throw new Error('Invalid credentials');
+      }
+    },
+  },
+});
+
+module.exports = User;
