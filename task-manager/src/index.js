bbe5217e5e5c1bbf6a355b7f6ffa243e37e64734
Josh Carter <joshcarter8400@gmail.com>
2021-03-13 18:28:29 -0500

    added in all authentication middleware, tokens, and postman urls

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 require('./db/mongoose');
 
-const Task = require('./models/Task');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 const app = express();
 
 const port = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -14,3 +14,18 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log('Server is listening on ' + port);
 });
+
+const Task = require('./models/Task');
+const User = require('./models/User');
+
+const main = async () => {
+  // const task = await Task.findById('604d3b4a6f000b519c44bca7');
+  // await task.populate('owner').execPopulate();
+  // console.log(task.owner);
+
+  const user = await User.findById('604d3a8ded99cb31a4b57330');
+  await user.populate('tasks').execPopulate();
+  console.log(user.tasks);
+};
+
+main();
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,27 +1,10 @@
 const express = require('express');
 const User = require('../models/User');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
-
-router.get('/users/:id', async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.post('/users', async (req, res) => {
@@ -29,13 +12,52 @@ router.post('/users', async (req, res) => {
 
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.patch('/users/:id', async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token != req.token;
+    });
+
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+
+    res.status(200).send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'email', 'password', 'age'];
   const isValidOperation = updates.every((update) =>
@@ -47,26 +69,19 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+
+    res.send(req.user);
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
+    await req.user.remove();
+    res.send(req.user);
   } catch (e) {
     res.status(400).send(e);
   }
