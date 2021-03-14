4f9181e7043e26da61879f6b05d2f082820e22c2
Josh Carter <joshcarter8400@gmail.com>
2021-03-14 14:57:35 -0400

    added in file uploading, image cropping, and all validation

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,18 +14,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log('Server is listening on ' + port);
 });
-
-// const Task = require('./models/Task');
-// const User = require('./models/User');
-
-// const main = async () => {
-//   // const task = await Task.findById('604d3b4a6f000b519c44bca7');
-//   // await task.populate('owner').execPopulate();
-//   // console.log(task.owner);
-
-//   const user = await User.findById('604d3a8ded99cb31a4b57330');
-//   await user.populate('tasks').execPopulate();
-
-// };
-
-// main();
